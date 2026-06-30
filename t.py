da16b67cceeb28bfb3f1d4b88f2fa905c9b9c7ae
Vedant Bhor <vedant23bhor@gmail.com>
2026-07-01 03:10:42 +0530

    first commit

---
+++ t.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# titanic_pipeline.py
+# Run in Kaggle or locally (pip install xgboost shap seaborn if missing)
+
+import pandas as pd
+import numpy as np
+import re
+from sklearn.model_selection import StratifiedKFold, cross_val_score, train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, OrdinalEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, classification_report
+import matplotlib.pyplot as plt
+import seaborn as sns
+
